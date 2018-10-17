b161b0fdc4ea39aa646651ad37d40a436caa4ca4
scottasc <scottschulfer@gmail.com>
2018-10-17 16:25:20 -0500

    class work

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
+
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+     <h1>People: {{people.length}}</h1>
+
+      <div v-for="person in people">
+          <h2 v-on:click="toggleBio(person)">{{person.name}}</h2>
+
+         <div v-if="person.bioVisible">
+          <h3>{{person.bio}}</h3>
+          <button v-on:click="deletePerson(person)">delete</button>
+         </div>
+
+      </div>
+
+         <div>
+          Name: <input v-model="newPerson.name">
+          Bio: <input v-model="newPerson.bio">
+          <button v-on:click="addPerson()">Add person</button>
+        </div>
+
+
   </div>
+
 </template>
 
+<style>
+  
+  .strike {
+    text-decoration: line-through;
+  }
+
+
+
+</style>
+
 <script>
+var axios = require('axios');
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function() {
+    return {
+      people: [],
+      newPerson: {name: "", bio: "", bioVisible: true}
+    };
+  },
+  created: function() {
+    axios
+    .get("http://localhost:3000/api/people")
+    .then(function(response) {
+      this.people = response.data; // this points to an undefined place because it's in an anonymous function and nested away from a direct reference to the Vue object.
+    }
+    .bind(this));
+  },
+  methods: {
+    addPerson: function() {
+      if (this.newPerson.name && this.newPerson.bio) {
+        this.people.push(this.newPerson);
+        this.newPerson = {name: "", bio: "", completed: false};
+    }
+  },
+    toggleBio: function(inputPerson) {
+      inputPerson.bioVisible = !inputPerson.bioVisible;
+    },
+    deletePerson: function (inputPerson) {
+      var index = this.people.indexOf(inputPerson);
+      this.people.splice(index, 1);
+    }
+  },
+  computed: {}
+};
 </script>
